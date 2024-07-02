a7cb519f3a81353a770b8715a870c5a7fa92931c
James Loh <lohjames16@gmail.com>
2024-07-02 15:01:51 -0500

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -11,19 +11,10 @@ export default function App() {
       <div className="w-screen flex flex-col">
         <div className="flex w-full">
           <div className=" flex w-1/2 mr-6">
-            <div className=" flex flex-col mt-10 pt-10 w-full h-full">
-              <div className="mx-5 flex flex-col justify-center items-center">
-                <div className="w-[750px] h-[550px] outline outline-gray-200 flelx flex-col justify-center p-2">
-                  <img
-                    width={735}
-                    height={700}
-                    src={default_room}
-                    alt="default room"
-                  />
-                  <div className="flex"></div>
-                </div>
-              </div>
-            </div>
+            <ImageScreen
+              image={default_room}
+              caption="Traditional / Living Room"
+            />
           </div>
           <div className=" flex w-1/2">
             <RecProducts></RecProducts>
